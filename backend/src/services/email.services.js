8baef062bdb96ebedf0a4443033f778c24392cc1
Shishank <shishankshakher@gmail.com>
2026-02-12 13:28:58 +0530

    Replaced SMTP from Nodemailer to RESEND

--- backend/src/services/email.services.js
+++ backend/src/services/email.services.js
@@ -1,14 +1,27 @@
 import { transporter } from "../config/transporter.js";
 import jwt from "jsonwebtoken";
+import { Resend } from "resend";
+
+// export const sendOtpEmail = async (email, otp) => {
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: "Your OTP Code",
+//     text: `Your OTP is ${otp}. It expires in 5 minutes.`,
+//   });
+// };
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (email, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to: email,
     subject: "Your OTP Code",
-    text: `Your OTP is ${otp}. It expires in 5 minutes.`,
+    html: `<p>Your OTP is <strong>${otp}</strong>. It expires in 5 minutes.</p>`,
   });
 };
+
 
 export const authenticateToken = (req, res, next) => {
   // Get token from Authorization header
